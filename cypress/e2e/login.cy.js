04573fbaea71332d02467ef560c4cfc0f76c5abc
Jolie VO <hieuvtk1207@gmail.com>
2025-07-04 17:18:28 +0700

    screenshot

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,8 +1,8 @@
 describe('Login Flow', () => {
   it('logs in successfully with correct credentials', () => {
     cy.visit('http://localhost:3000');
-    cy.get('#username').type('admin');
-    cy.get('#password').type('123');
+    cy.get('#username').type('admin123');
+    cy.get('#password').type('123123');
     cy.get('button').click();
     cy.contains('Login successful');
   });
